aafcc0c20f960a70531e77208a2616f371b47cdd
1313Ben <ravindra_github@bluewin.ch>
2023-03-30 09:56:07 +0200

    Custom style and initial contents

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 var app = new Vue({
   el: '#app',
   data: {
+    tasks: [
+      { id: 1, name: 'Todo 1', description: 'This is a todo', completed: false},
+      { id: 2, name: 'Todo 2', description: 'This is a todo', completed: true},
+      { id: 3, name: 'Todo 3', description: 'This is a completed todo', completed: false},
+      { id: 4, name: 'Todo 4', description: 'This is a another completed todo', completed: true}
+    ],
     message: 'Hello Vue!'
+  },
+  computed: {
+    completedTasks: function () {
+      return this.tasks.filter(item => item.completed == true);
+    },
+    todoTasks: function () {
+      return this.tasks.filter(item => item.completed == false);
+    }
   }
 })
