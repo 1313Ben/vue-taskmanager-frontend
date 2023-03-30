e2aa569d9c11090df9bcd6624303b6f0afebfd1d
1313Ben <ravindra_github@bluewin.ch>
2023-03-30 10:03:50 +0200

    v-bind:class

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = new Vue({
     tasks: [
       { id: 1, name: 'Todo 1', description: 'This is a todo', completed: false},
       { id: 2, name: 'Todo 2', description: 'This is a todo', completed: true},
-      { id: 3, name: 'Todo 3', description: 'This is a completed todo', completed: false},
+      { id: 3, name: 'Todo 3', description: 'This is a completed todo', completed: true},
       { id: 4, name: 'Todo 4', description: 'This is a another completed todo', completed: true}
     ],
     message: 'Hello Vue!'
