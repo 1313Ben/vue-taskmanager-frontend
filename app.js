261a8a7d6e81962fd8824ac4ea07c5f5a194f1da
1313Ben <ravindra_github@bluewin.ch>
2023-03-30 10:25:35 +0200

    Created Vue.component

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+Vue.component('task', {
+  props: ['task'],
+  template: `
+    <div class="ui segment task" v-bind:class="task.completed ? 'done' : 'todo' ">
+      {{ task.id }} {{ task.name }}: {{ task.description }} completed? {{
+      task.completed }}
+    </div>
+  `
+});
+
 var app = new Vue({
   el: '#app',
   data: {
