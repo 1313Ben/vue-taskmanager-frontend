924e6c489873ad4c5f76759d218da655bdcc7cd1
1313Ben <ravindra_github@bluewin.ch>
2023-03-30 12:05:08 +0200

    Toggling checkbox

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 Vue.component('task', {
-  props: ['task'],
+  props: ['task', 'tasks'],
   template: `
     <div class="ui segment task" v-bind:class="task.completed ? 'done' : 'todo' "> 
       <div class="ui grid">
         <div class="left floated twelve wide column">
           <div class="ui checkbox">
-            <input type="checkbox" name="task" :checked="task.completed" >
+            <input type="checkbox" name="task" v-on:click="toggleDone($event, task.id)" :checked="task.completed" >
             <label>{{task.name}} <span class="description">{{task.description}}</span></label>
           </div>
         </div>
@@ -13,7 +13,18 @@ Vue.component('task', {
         </div>
       </div>
     </div>
-  `
+    `,
+  methods: {
+    toggleDone: function (event, id) {
+      //console.log(event);
+      //console.log(id);
+      let task = this.tasks.find(item => item.id == id)
+      if (task) {
+        task.completed = !task.completed;
+        console.log("task toggled");
+      }
+    }
+  }
 });
 
 
@@ -25,8 +36,7 @@ var app = new Vue({
       { id: 2, name: 'Todo 2', description: 'This is a todo', completed: true},
       { id: 3, name: 'Todo 3', description: 'This is a completed todo', completed: true},
       { id: 4, name: 'Todo 4', description: 'This is a another completed todo', completed: true}
-    ],
-    message: 'Hello Vue!'
+    ]
   },
   computed: {
     completedTasks: function () {
@@ -35,5 +45,6 @@ var app = new Vue({
     todoTasks: function () {
       return this.tasks.filter(item => item.completed == false);
     }
-  }
+  },
+  
 })
